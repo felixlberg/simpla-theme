f712cc320ae62828c0af6314dd91fdcb95b1806b
Felix <felix.lberg@gmail.com>
2019-03-09 01:28:23 +0100

    changed netlify adapter

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const SIMPLA_CONFIG = {
   repo: 'felixlberg/felixlberg.github.io',
-  auth: new SimplaNetlify({ site: 'felixlberg' }),
+  auth: new SimplaNetlify({ site: 'dreamy-hoover-1e7bc7.netlify.com' }),
   source: window.location.origin,
   branch: 'master'
 };
