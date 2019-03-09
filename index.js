7430ce8e348e98da879b738c4810ab94be00e725
Felix <felix.lberg@gmail.com>
2019-03-09 02:11:51 +0100

    added demo mode to index.js

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ const SIMPLA_CONFIG = {
   repo: 'felixlberg/felixlberg.github.io',
   auth: new SimplaNetlify({ site: 'dreamy-hoover-1e7bc7.netlify.com' }),
   source: window.location.origin,
-  branch: 'master'
+  branch: 'master',
+  public: 'dist'
 };
 
 // Init Simpla
 Simpla.init(SIMPLA_CONFIG);
 window.Simpla = Simpla;
 
-// Prompt for login from auth adapter
-Simpla.login();
+// Save utility method
+// We're not using simpla-admin due to demo mode
+window.SimplaJS = {};
+window.SimplaJS.save = () => {
+  const loginThenSave = () => {
+    if (!Simpla.getState('authenticated')) {
+      return Simpla.login().then(() => Simpla.save());
+    }
 
-// Log user out locally
-Simpla.logout();
+    return Simpla.save();
+  };
+
+  loginThenSave()
+    .then(() => alert('Content saved!'))
+    .catch(err => alert(`Save failed, ${err}`));
+};
