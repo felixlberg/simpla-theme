e6c80166ea519ee6f2bc90efe05c7dd1e0060d09
Felix <felix.lberg@gmail.com>
2019-03-10 15:57:22 +0100

    testing

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const SIMPLA_CONFIG = {
   repo: 'felixlberg/felixlberg.github.io',
   auth: new SimplaNetlify({ site: 'dreamy-hoover-1e7bc7.netlify.com' }),
   source: window.location.origin,
-  branch: 'master',
-  public: 'dist'
+  branch: 'master'
 };
 
 // Init Simpla
