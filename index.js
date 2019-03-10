6ffa43888229448584b31cf611f4e4e223a74b5e
Felix <felix.lberg@gmail.com>
2019-03-10 15:18:03 +0100

    added public for path error

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const SIMPLA_CONFIG = {
   repo: 'felixlberg/felixlberg.github.io',
   auth: new SimplaNetlify({ site: 'dreamy-hoover-1e7bc7.netlify.com' }),
   source: window.location.origin,
-  branch: 'master'
+  branch: 'master',
+  public: 'dist'
 };
 
 // Init Simpla
